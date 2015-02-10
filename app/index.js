b2625d1d4176398b62dab59d9593cef0bb665f83
Jad Joubran <jad@jadjoubran.com>
2015-02-09 20:08:41 -0800

    checking for composer & bower

--- app/index.js
+++ app/index.js
@@ -1,62 +1,90 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+var yeoman = require('yeoman-generator'),
+chalk  = require('chalk'),
+cp  = require('child_process');
 
 module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../package.json');
-  },
+	initializing: function () {
+		this.pkg = require('../package.json');
+	},
 
-  prompting: function () {
-    var done = this.async();
+	prompting: function () {
+		var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the cool' + chalk.red('Laravel5') + ' generator!'
-    ));
+		this.log(chalk.red('Laravel5') + ' generator will install laravel5 in your folder!');
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+		var prompts = [{
+			type: 'confirm',
+			name: 'someOption',
+			message: 'Would you like to enable this option?',
+			default: true
+		}];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+		this.prompt(prompts, function (props) {
+			this.someOption = props.someOption;
 
-      done();
-    }.bind(this));
-  },
+			done();
+		}.bind(this));
+	},
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
-    },
+	configuring: {
+		composer: function(){
+			this.log(chalk.cyan('Testing for composer'));
+			var self = this;
+			cp.exec('composer --version', function (err, stdout, stderr) {
+				if (err) {
+					self.log(chalk.red('Composer not found. Make sure it is available in your path and download it from https://getcomposer.org'));
+					return false;
+				}
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+				self.log(chalk.green('Composer found'));
+			});
+},
+		bower: function(){
+			this.log(chalk.cyan('Testing for bower'));
+			var self = this;
+			cp.exec('bower -v', function (err, stdout, stderr) {
+				if (err) {
+					self.log(chalk.cyan('Bower not found. Installing bower'));
+					cp.exec('npm install -g bower', function(err, stdout, stderr){
+						self.log(stdout);
+					});
+				}
 
-  install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
-  }
+				self.log(chalk.green('Bower found'));
+			});
+},
+
+
+},
+
+writing: {
+	app: function () {
+		this.fs.copy(
+			this.templatePath('_package.json'),
+			this.destinationPath('package.json')
+			);
+		this.fs.copy(
+			this.templatePath('_bower.json'),
+			this.destinationPath('bower.json')
+			);
+	},
+
+	projectfiles: function () {
+		this.fs.copy(
+			this.templatePath('editorconfig'),
+			this.destinationPath('.editorconfig')
+			);
+		this.fs.copy(
+			this.templatePath('jshintrc'),
+			this.destinationPath('.jshintrc')
+			);
+	}
+},
+
+install: function () {
+	this.installDependencies({
+		skipInstall: this.options['skip-install']
+	});
+}
 });
